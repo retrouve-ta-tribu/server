c6d3abde5b313786a1b8573e2d1358b7afdd7d80
Benjamin Fontana <ben192003@gmail.com>
2025-03-03 16:14:02 +0100

    refactor: improve mongodb configuration and connection setup

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("MongoDB connected successfully");
+      const {
+          MONGO_USERNAME,
+          MONGO_PASSWORD,
+          MONGO_PORT = 27017,
+          MONGO_HOST = "localhost",
+          MONGO_DATABASE,
+      } = process.env;
+      const mongoURI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
+      await mongoose.connect(mongoURI);
+      console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
     process.exit(1);
