13bfedaedf58414657ad3560a2507124a22e96d3
Benjamin Fontana <ben192003@gmail.com>
2025-03-10 15:15:29 +0100

    feat: setup express routes configuration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+import groupRoutes from "./routes/groupRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import pointOfInterestRoutes from "./routes/pointOfInterestRoutes";
 
 dotenv.config();
 connectDB();
@@ -9,5 +13,10 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/groups', groupRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/points', pointOfInterestRoutes);
 
 export default app;
